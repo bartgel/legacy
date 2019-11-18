5ae7f378d2eee7f189ed8169283a8c2fe6c6735b
Bart Geluykens <bartgeluykens@gmail.com>
2019-11-18 21:32:58 +0100

    Added configurable hello world

--- src/hello.c
+++ src/hello.c
@@ -1,6 +1,8 @@
 #include "signs.h"
+#include <stdio.h>
+#include <string.h>
 
-int main(void) {
+int default_hello(void) {
 	exm(0);
 	h(1);
 	e(0);
@@ -14,4 +16,25 @@ int main(void) {
 	d(0);
 	exm(0);
 	cr(0);
+}
+
+char* getFileName(char* filename, size_t fileSize, int argnum, char** args) {
+	memset(filename, 0, fileSize);
+	strcpy(filename,"conf/");
+        strncat(filename, argnum == 1 ? "default.txt": args[1],fileSize -1);
+	return filename;
+}
+
+int main(int argnum, char** args) {
+	char filename[300];
+	FILE* fp = fopen(getFileName(filename,sizeof(filename),argnum,args),"r");
+	char buff[255];
+	if (fp != 0) {
+		memset(buff, 0, sizeof(buff));
+		fgets(buff,sizeof(buff) -1,fp);
+		printf("%s",buff);
+		fclose(fp);
+		return 0;
+	}
+	default_hello();
 }
