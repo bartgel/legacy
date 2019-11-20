4a56036cdf6bf02a2357840b825d64f6eba4d983
Bart Geluykens <bartgeluykens@gmail.com>
2019-11-20 21:35:42 +0100

    Added simple http server

--- src/hello.c
+++ src/hello.c
@@ -1,4 +1,5 @@
 #include "signs.h"
+#include "httpd.h"
 #include <stdio.h>
 #include <string.h>
 
@@ -25,7 +26,7 @@ char* getFileName(char* filename, size_t fileSize, int argnum, char** args) {
 	return filename;
 }
 
-int main(int argnum, char** args) {
+int omain(int argnum, char** args) {
 	char filename[300];
 	FILE* fp = fopen(getFileName(filename,sizeof(filename),argnum,args),"r");
 	char buff[255];
@@ -37,4 +38,32 @@ int main(int argnum, char** args) {
 		return 0;
 	}
 	default_hello();
+}
+
+
+
+int main(int c, char** v)
+{
+    serve_forever("8080");
+    return 0;
+}
+
+void route()
+{
+    ROUTE_START()
+
+    ROUTE_GET("/")
+    {
+        printf("HTTP/1.1 200 OK\r\n\r\n");
+        printf("Hello! You are using %s", request_header("User-Agent"));
+    }
+
+    ROUTE_POST("/")
+    {
+        printf("HTTP/1.1 200 OK\r\n\r\n");
+        printf("Wow, seems that you POSTed %d bytes. \r\n", payload_size);
+        printf("Fetch the data using `payload` variable.");
+    }
+  
+    ROUTE_END()
 }
