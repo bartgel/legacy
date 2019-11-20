6278c033f19b50f438fd2eb75cd5b2fce7860bf5
Bart Geluykens <bartgeluykens@gmail.com>
2019-11-20 21:53:58 +0100

    Combine hello world and http servlet

--- src/hello.c
+++ src/hello.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <string.h>
 
+int argnum;
+char** args;
 int default_hello(void) {
 	exm(0);
 	h(1);
@@ -26,24 +28,12 @@ char* getFileName(char* filename, size_t fileSize, int argnum, char** args) {
 	return filename;
 }
 
-int omain(int argnum, char** args) {
-	char filename[300];
-	FILE* fp = fopen(getFileName(filename,sizeof(filename),argnum,args),"r");
-	char buff[255];
-	if (fp != 0) {
-		memset(buff, 0, sizeof(buff));
-		fgets(buff,sizeof(buff) -1,fp);
-		printf("%s",buff);
-		fclose(fp);
-		return 0;
-	}
-	default_hello();
-}
-
 
 
 int main(int c, char** v)
 {
+	argnum = c;
+	args=v;
     serve_forever("8080");
     return 0;
 }
@@ -54,8 +44,19 @@ void route()
 
     ROUTE_GET("/")
     {
+	char filename[300];
+	FILE* fp = fopen(getFileName(filename,sizeof(filename),argnum,args),"r");
+	char buff[255];
         printf("HTTP/1.1 200 OK\r\n\r\n");
-        printf("Hello! You are using %s", request_header("User-Agent"));
+	if (fp != 0) {
+		memset(buff, 0, sizeof(buff));
+		fgets(buff,sizeof(buff) -1,fp);
+		printf("%s",buff);
+		fclose(fp);
+		return;
+	}
+	default_hello();
+
     }
 
     ROUTE_POST("/")
